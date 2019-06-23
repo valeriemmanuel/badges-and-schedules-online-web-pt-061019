d6019ec302d569c083d339526791acb58327ff22
Valerie Manuel <valerie.manuel11@gmail.com>
2019-06-23 01:34:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+let(:attendees) {["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]}
+  let(:badges) {[
+                  "Hello, my name is Edsger.",
+                  "Hello, my name is Ada.",
+                  "Hello, my name is Charles.",
+                  "Hello, my name is Alan.",
+                  "Hello, my name is Grace.",
+                  "Hello, my name is Linus.",
+                  "Hello, my name is Matz."
+               ]}# Write your code here.
