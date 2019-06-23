9abc8f5a72517a962e91b9a24bfa66e95a37fdbc
Valerie Manuel <valerie.manuel11@gmail.com>
2019-06-23 01:41:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,15 +1,9 @@
 attendees = {"Edsger", "Ada", "Charles", "Alan", "Grace",
 "Linus", "Matz"}
   attendees.each do |name|
-    puts 
+    puts "Hello, my name is #{attendees}!"
     
     
   end
-                  "Hello, my name is Edsger.",
-                  "Hello, my name is Ada.",
-                  "Hello, my name is Charles.",
-                  "Hello, my name is Alan.",
-                  "Hello, my name is Grace.",
-                  "Hello, my name is Linus.",
-                  "Hello, my name is Matz."
+                
             
