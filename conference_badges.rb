bcd5ebd22f6529ded2c363003d1533a9a6a148d8
Valerie Manuel <valerie.manuel11@gmail.com>
2019-06-23 01:46:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,7 @@
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace",
 "Linus", "Matz"]
+
+
     brothers.each{|name| puts "Hello, my name is #{attendees}!"}
     puts "Hello, my name is #{attendees}!"
   end
