7f2adfe6bbb96792fcbb5f8c44bb71169787ee6e
Valerie Manuel <valerie.manuel11@gmail.com>
2019-06-23 01:50:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@
 attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace",
 "Linus", "Matz"]
 
-batch_badge_creater (attendees)
+batch_badge_creater [attendees]
     attendees.each{|name| puts "Hello, my name is #{attendees}!"}
   
                 
